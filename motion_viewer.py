3739cffd4e10f3bfcb27dcdfa34cac81f660ad97
Chrystiano Araujo <chrystianosaraujo@gmail.com>
2017-11-30 16:28:09 -0800

    Replacing pygame by PyGlet. The problem of running MotionViewer with Intel or ATI GPU has been solved with this modification.

--- motion_viewer.py
+++ motion_viewer.py
@@ -1,28 +1,36 @@
-import OpenGL.GL as GL
-import pygame
-import glm
+from OpenGL.GL import *
+from pyglet.window import key as KEY
+from pyglet.window import mouse as MOUSE
+import pyglet
 import sys
+import glm
 
 from motion_render import MotionRender
 
+class MotionViewer(pyglet.window.Window):
+    def __init__(self, window_size = (800, 600)):
+        self._window_size = window_size
 
-class MotionViewer:
-    WINDOW_TITLE = "CPSC 526 - Final Project: Chrystiano Araujo & Edoardo Dominici"
+        caption = "CPSC 526 - Final Project: Chrystiano Araujo & Edoardo Dominici"
+        vsync_enabled = True
 
-    def __init__(self, screen_size = (800, 600)):
-        self._screen_size = screen_size
+        init_data = {
+          "width"    : window_size[0],
+          "height"   : window_size[1],
+          "caption"  : caption,
+          "resizable": True,
+          "vsync"    : vsync_enabled,
+        }
+
+        super(MotionViewer, self).__init__(**init_data)
+
+        self._fps_display = pyglet.clock.ClockDisplay()
+
         self._events_cb = {}
         self._background_color = [0.0, 0.0, 0.0, 0.0]
 
-        self._register_events()
-        self._setup_window()
         self._setup_gl()
         self._setup_renderers()
-
-    def run(self):
-        while True:
-            self._handle_events()
-            self._draw_scene()
 
     def shutdown(self, **_):
         self._motion_render.clean_up()
@@ -30,74 +38,52 @@ class MotionViewer:
         # TODO: Any state must be saved here
         sys.exit()
 
-    def _setup_window(self):
-        pygame.init()
-
-        flags = pygame.OPENGL | pygame.DOUBLEBUF
-        self.screen = pygame.display.set_mode(self._screen_size, flags)
-        pygame.display.set_caption(MotionViewer.WINDOW_TITLE)
-
-        # Used to control the framerate
-        self.clock = pygame.time.Clock()
-
-    def _setup_gl(self):
-        GL.glClearColor(*self._background_color)
-
-        aspect = self._screen_size[0] / float(self._screen_size[1])
-        self._projection_matrix = glm.perspective(45.0,
-                                                  aspect,
-                                                  0.00001,
-                                                  100.0)
-
-        self._view_matrix = glm.lookAt(glm.vec3(-1.5,  -1.5, -1.0),
-                                       glm.vec3(0.0,  0.0,  0.0),
-                                       glm.vec3(0.0,  1.0,  0.0))
-
-    def _setup_renderers(self):
-        self._motion_render = MotionRender()
-
-    def _register_events(self):
-        self._events_cb[pygame.QUIT]            = self.shutdown
-        self._events_cb[pygame.VIDEORESIZE]     = self._resize
-        self._events_cb[pygame.MOUSEBUTTONDOWN] = self._handle_mouse_down
-        self._events_cb[pygame.MOUSEBUTTONUP]   = self._handle_mouse_up
-        self._events_cb[pygame.MOUSEMOTION]     = self._handle_mouse_motion
-        self._events_cb[pygame.KEYDOWN]         = self._handle_key_press
-
-    def _handle_events(self):
-        for e in pygame.event.get():
-            if e.type in self._events_cb:
-                self._events_cb[e.type](**e.dict)
-
-    def _resize(self, size, **_):
-        GL.glViewport(0, 0, size[0], size[1])
-
-    def _handle_mouse_down(self, pos, button, **_):
-        pass
-
-    def _handle_mouse_up(self, pos, button, **_):
-        pass
-
-    def _handle_mouse_motion(self, pos, rel, buttons, **_):
-        pass
-
-    def _handle_key_press(self, key, mod, **_):
-        if key in (pygame.K_ESCAPE, pygame.K_q):
-            self.shutdown()
-
-    def _draw_scene(self):
-        time_passed = self.clock.tick()
+    def on_draw(self):
+        self.clear()
 
         # Update model, view, and project matrices in all renderers
         # TODO: replace this for a camera manipulator
         self._motion_render.set_render_matrices(self._view_matrix, self._projection_matrix)
 
-        GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         self._motion_render.draw()
-        #floor_render.draw()
+        self._fps_display.draw()
 
-        pygame.display.flip()
+    def _setup_renderers(self):
+        self._motion_render = MotionRender()
 
-if __name__ == "__main__":
+    def _setup_gl(self):
+        glClearColor(*self._background_color)
+
+        aspect = self._window_size[0] / float(self._window_size[1])
+        self._projection_matrix = glm.perspective(45.0,
+                                                  aspect,
+                                                  0.00001,
+                                                  100.0)
+
+        self._view_matrix = glm.lookAt(glm.vec3(-1.5, -1.5, -1.0),
+                                       glm.vec3( 0.0,  0.0,  0.0),
+                                       glm.vec3( 0.0,  1.0,  0.0))
+
+    def on_resize(self, width, height):
+        glViewport(0, 0, width, height)
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol in (KEY.ESCAPE, KEY.Q):
+            self.shutdown()
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        See http://pyglet.readthedocs.io/en/pyglet-1.3-maintenance/programming_guide/mouse.html
+        """
+        pass
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        pass
+
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        pass
+
+if __name__ == '__main__':
     viewer = MotionViewer()
-    viewer.run()
+    pyglet.app.run()
