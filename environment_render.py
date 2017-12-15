Fixed depth artifacts. Added transition rotation alignment. Faster motion graph construction.

environment_render.py
@@ -93,10 +93,10 @@ class EnvironmentRender:
         proj_loc = self._shader_program.uniform_location('projectionMatrix')
         sampler_loc = self._shader_program.uniform_location('color_map')
 
-        FLOOR_SIZE = 2
+        FLOOR_SIZE = 1
         scale = glm.scale(glm.mat4(), glm.vec3(FLOOR_SIZE, 1, FLOOR_SIZE)) * glm.translate(glm.mat4(), glm.vec3(0.0, -20.0, 0.0))
 
-        GL.glUniformMatrix4fv(model_loc, 1, GL.GL_FALSE, np.ascontiguousarray(glm.mat4()))
+        GL.glUniformMatrix4fv(model_loc, 1, GL.GL_FALSE, np.ascontiguousarray(scale))
         GL.glUniformMatrix4fv(view_loc, 1, GL.GL_FALSE, np.ascontiguousarray(self._view_matrix))
         GL.glUniformMatrix4fv(proj_loc, 1, GL.GL_FALSE, np.ascontiguousarray(self._proj_matrix))
 

motion_graph.py
@@ -145,13 +145,13 @@ class MotionGraph:
         """
         self._set_progress_cb(progress_cb)
 
-        pr = debugger.start_profiler()
+        # pr = debugger.start_profiler()
 
         self._build_similarity_matrix()
         self._find_local_minima()
         self._generate_graph()
 
-        debugger.finish_profiler(pr)
+        # debugger.finish_profiler(pr)
 
         self._set_progress_cb(None)
 
@@ -407,8 +407,6 @@ class MotionGraph:
         frames_i = self._get_motion_window_as_hierarchical_poses(*window_i)
         frames_j = self._get_motion_window_as_hierarchical_poses(*window_j)
 
-        #frame_i_positions = self._frames_pose_positions[window_i[0]]
-        #frame_j_positions = self._frames_pose_positions[window_j[0]]
         motion_i = self._motion_data_containing_frame(window_i[0])
         motion_j = self._motion_data_containing_frame(window_j[0])
         motion_frame_i = self._motion_frame_number(motion_i, window_i[0])
@@ -416,16 +414,19 @@ class MotionGraph:
         frame_i_positions = motion_i.get_frames_positions(motion_frame_i)
         frame_j_positions = motion_j.get_frames_positions(motion_frame_j)
 
-        angle, tx, tz = self._compute_alignment_between_frames(frame_i_positions, frame_j_positions)
-        _, tx, tz = 0.0, 0.0, 0.0
-        #angle = 0.0
+        diff = frames_i[0].position - frames_j[0].position
+        tx, _, tz = diff
+
+        dir_i = frames_i[-1].position - frames_i[0].position
+        dir_j = frames_j[-1].position - frames_i[0].position
+        dir_i[1] = 0.0
+        dir_j[1] = 0.0
+        dir_i_len = np.linalg.norm(dir_i)
+        dir_j_len = np.linalg.norm(dir_j)
+        angle = math.acos(np.dot(dir_i / dir_i_len, dir_j / dir_j_len))
 
         align_trans = np.array([tx, 0.0, tz])
         align_rot   = Quaternion(axis=[0.0, 1.0, 0.0], radians=angle)
-
-        diff = frames_i[0].position - frames_j[0].position
-        tx, _, tz = diff
-        angle = 0.0
 
         frames_j = copy.deepcopy(frames_j)
         blended_frames = copy.deepcopy(frames_i)
@@ -438,7 +439,7 @@ class MotionGraph:
             pose_i, pose_j, pose_b = poses
 
             # Align pose_j in respect to pose_i
-            pose_j.position = np.add(pose_j.position, np.array([tx, 0.0, tz]));
+            pose_j.position = np.add(pose_j.position, align_trans)
             pose_j.offset   = np.array([0.0, 0.0, 0.0])
 
             roll, pitch, yaw = pose_j.angles
@@ -704,7 +705,6 @@ class MotionGraph:
             tags = all_tags[ii]
 
             # Counting components
-            import ipdb; ipdb.set_trace()
             component_nodes = {}
             for node, component in tags[0].items():
                 if component not in component_nodes:

motion_render.py
@@ -32,7 +32,6 @@ class MotionRender:
         self._joints_color = glm.vec4(200.0 / 255.0, 200.0 / 255.0,
                                       200.0 / 255.0, 1.0)
         self._uniforms = {}
-        self._debug_characters = []
 
         self._setup_render_matrices()
         self._setup_ligthing()
@@ -64,7 +63,7 @@ class MotionRender:
 
         # Draw joints
         # Ignoring the following parts
-        #
+        
         # NodeType.UPPER_ARM: RightShoulder
         # NodeType.UPPER_ARM: LeftShoulder
         # NodeType.TORSO    : RHipJoint
@@ -85,49 +84,7 @@ class MotionRender:
     def set_color(self, color):
         self._character_color = color
 
-    def draw_debug(self):
-        self._uniforms = {
-            'model_mat_loc': self._shader_program.uniform_location("modelMatrix"),
-            'view_mat_loc': self._shader_program.uniform_location("viewMatrix"),
-            'proj_mat_loc': self._shader_program.uniform_location("projectionMatrix"),
-            'normal_mat_loc': self._shader_program.uniform_location("normalMatrix"),
-            'character_color_loc': self._shader_program.uniform_location(":haracterColor"),
-            #'ambient_color_loc': self._shader_program.uniform_location("ambientColor"),
-            #'specular_color_loc': self._shader_program.uniform_location("specularColor"),
-            #'shininess_loc': self._shader_program.uniform_location("shininess")
-        }
-
-        # Setting all shared data
-        self._shader_program.bind()
-        GL.glEnable(GL.GL_DEPTH_TEST)
-
-        GL.glUniformMatrix4fv(self._uniforms['view_mat_loc'], 1, GL.GL_FALSE, np.ascontiguousarray(self._view_matrix))
-        GL.glUniformMatrix4fv(self._uniforms['proj_mat_loc'], 1, GL.GL_FALSE, np.ascontiguousarray(self._proj_matrix))
-        GL.glUniform4fv(self._uniforms['character_color_loc'], 1, np.ascontiguousarray(self._character_color))
-        #GL.glUniform4fv(self._uniforms['diffuse_color_loc'], 1, np.ascontiguousarray(self._diffuse_color))
-        #GL.glUniform4fv(self._uniforms['specular_color_loc'], 1, np.ascontiguousarray(self._specular_color))
-        #GL.glUniform1f(self._uniforms['shininess_loc'], self._shininess)
-
-
-        color = [
-            glm.vec4(1.0, 0.0, 0.0, 1.0),
-            glm.vec4(0.0, 1.0, 0.0, 1.0),
-            glm.vec4(0.0, 0.0, 1.0, 1.0),
-        ]
-
-        if self._debug_characters:
-            for ii, (edge, t) in enumerate(self._debug_characters):
-#                root_transform = glm.translate(glm.mat4(), glm.vec3(ii * 10, 0.0, ii * 10))
-                self.set_color(color[ii])
-                for jj, frame in enumerate(edge.frames):
-                    if jj % 5 == 0:
-                        edge.motion.traverse(frame, self._on_draw_part, t)
-
-
     def draw(self, frame):
-        if self._debug_characters:
-            elf.draw_debug()
-
         if self._skeleton is None:
             return
 