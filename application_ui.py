5bf2f5ca3cb63275e2ffbfefd226856856f0e4b9
Edoardo Dominici <edoaramis@gmail.com>
2017-12-14 18:39:04 -0800

    Working on UI: Loading and playback

--- application_ui.py
+++ application_ui.py
@@ -8,53 +8,138 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_MainWindow(object):
+import os
+
+WIDTH = 1920
+HEIGHT = 1080
+
+class UIMainWindow(object):
+    def __init__(self):
+        # Any class that implements all on_* methods
+        self.callback_handler = None
+        self._motions = []
+
     def setupUi(self, MainWindow):
+        self.main_window = MainWindow
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1848, 1165)
+        MainWindow.resize(WIDTH, HEIGHT)
         MainWindow.setMinimumSize(QtCore.QSize(800, 600))
-        MainWindow.setStyleSheet("background-color: rgb(0, 0, 0);")
+       
         self.centralwidget = QtWidgets.QWidget(MainWindow)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Ignored)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
-        self.centralwidget.setSizePolicy(sizePolicy)
         self.centralwidget.setObjectName("centralwidget")
+
         self.viewer_widget = MotionViewWidget(self.centralwidget)
-        self.viewer_widget.setGeometry(QtCore.QRect(0, 0, 1920, 1080))
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Ignored)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.viewer_widget.sizePolicy().hasHeightForWidth())
         self.viewer_widget.setSizePolicy(sizePolicy)
-        self.viewer_widget.setMinimumSize(QtCore.QSize(1500, 0))
+        self.viewer_widget.setMinimumSize(QtCore.QSize(WIDTH * 0.66, HEIGHT))
         self.viewer_widget.setStyleSheet("")
         self.viewer_widget.setObjectName("viewer_widget")
-        self.graph_widget = MotionGraphVisualizerWidget(self.viewer_widget)
-        self.graph_widget.setGeometry(QtCore.QRect(0, 0, 800, 600))
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        
+        self.graph_widget = MotionGraphVisualizerWidget(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.graph_widget.sizePolicy().hasHeightForWidth())
         self.graph_widget.setSizePolicy(sizePolicy)
-        self.graph_widget.setMinimumSize(QtCore.QSize(0, 0))
         self.graph_widget.setObjectName("graph_widget")
+
+        self.list_widget = QtWidgets.QListWidget(self.centralwidget)
+
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1848, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+
+        self.grid_layout = QtWidgets.QGridLayout(self.centralwidget)
+        self.grid_layout.addWidget(self.viewer_widget, 0, 0, 2, 1)
+        self.grid_layout.addWidget(self.graph_widget, 0, 1, 1, 1)
+        self.grid_layout.addWidget(self.list_widget, 1, 1, 1, 1)
+        self.grid_layout.setContentsMargins(0, 0, 0, 0)
+        self.grid_layout.setSpacing(0)
+
+        open_file_a = QtWidgets.QAction('Open .BVH', MainWindow)
+        open_file_a.setShortcut('Ctrl+O')
+        open_file_a.setStatusTip('Import .BVH')
+        open_file_a.triggered.connect(self._on_open_file)
+
+        exit_a = QtWidgets.QAction('Exit', MainWindow)
+        exit_a.setShortcut('Alt+F4')
+
+        file_menu = MainWindow.menuBar().addMenu('File')
+        file_menu.addAction(open_file_a)
+        file_menu.addSeparator()
+        file_menu.addAction(exit_a)
+
+        generate_motion_graph_a = QtWidgets.QAction('Generate Motion Graph', MainWindow)
+        generate_motion_graph_a.setShortcut('Ctrl+G')
+        generate_motion_graph_a.triggered.connect(self._on_generate_motion_graph)
+
+        motion_graph_menu = MainWindow.menuBar().addMenu('Motion Graph')
+        motion_graph_menu.addAction(generate_motion_graph_a)
+
+        about_a = QtWidgets.QAction('About', MainWindow)
+        about_a.triggered.connect(self._on_about)
+
+        help_menu = MainWindow.menuBar().addMenu('Help')
+        help_menu.addAction(about_a)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("Motion Viewer", "Motion Viewer"))
+
+    def _on_open_file(self):
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        path, _ = QtWidgets.QFileDialog.getOpenFileName(self.main_window, "QFileDialog.getOpenFileName()", "","All Files (*);;Biovision Hierarchy (*.bvh)", options=options)
+        if path:
+            if self.callback_handler:
+                motion = self.callback_handler.on_import_motion(path)
+                if motion:
+                    self._append_motion(path, motion)
+                    self._motions.append(motion)
+
+    def _on_generate_motion_graph(self):
+        pass
+
+    def _on_about(self):
+        pass
+
+    def _append_motion(self, path, motion):
+        item = QtWidgets.QListWidgetItem(self.list_widget)
+        item_widget = QtWidgets.QWidget(self.list_widget)
+        layout = QtWidgets.QHBoxLayout()
+
+        _, filename = os.path.split(path)
+
+        name = QtWidgets.QLabel(filename)
+        play_button = QtWidgets.QPushButton('play')
+        remove_button = QtWidgets.QPushButton('remove')
+
+        def on_play():
+            if self.callback_handler:
+                self.callback_handler.on_play_motion(motion)
+
+        def on_remove():
+            if self.callback_handler:
+                self.callback_handler.on_remove_motion(motion)
+                self.list_widget.takeItem(self._motions.index(motion))
+                self._motions.remove(motion)
+
+        play_button.clicked.connect(on_play)
+        remove_button.clicked.connect(on_remove)
+
+        layout.addWidget(name)
+        layout.addWidget(play_button)
+        layout.addWidget(remove_button)
+        layout.addStretch()
+        layout.setSizeConstraint(QtWidgets.QLayout.SetNoConstraint)
+        item_widget.setLayout(layout)
+        item.setSizeHint(item_widget.sizeHint())
+        self.list_widget.addItem(item)
+        self.list_widget.setItemWidget(item, item_widget)
 
 from motion_graph_widget import MotionGraphVisualizerWidget
 from motion_viewer_widget import MotionViewWidget
--- motion_viewer_app.py
+++ motion_viewer_app.py
@@ -3,36 +3,45 @@ import glm
 import pickle
 import os.path
 
-from application_ui import Ui_MainWindow
+from application_ui import UIMainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPalette, QColor
 
-from skeleton import AnimatedSkeleton
+from skeleton import AnimatedSkeleton, UnsupportedError, FormatError
 from motion_graph import MotionGraph
 from motion_graph_player import MotionGraphPlayer
+from motion_player import MotionPlayer
 
 MOTION_GRAPH_WINDOW_SIZE = 30
 GENERATE_GROUNDTRUTH_MOTION_GRAPH = False
+UPDATE_FREQUENCY = 120
+UPDATE_DT = 1.0 / UPDATE_FREQUENCY
 
 class MotionViewerApp:
     def __init__(self):
         self._app = QtWidgets.QApplication(sys.argv)
         self._main_window = QtWidgets.QMainWindow()
 
-        self._ui = Ui_MainWindow()
+        self._ui = UIMainWindow()
         self._ui.setupUi(self._main_window)
+
+        # Hooking up callbacks
+        self._ui.callback_handler = self
 
         self._update_timer = QtCore.QTimer()
         self._update_timer.timeout.connect(self.on_next_frame)
-        self._update_timer.start(1.0 / 120.0)
+        self._update_timer.start(UPDATE_DT)
 
+        self._setup_theme()
+
+        self._motion_player = MotionPlayer()
         self._motion_graph_player = MotionGraphPlayer()
-        self._prev_edge = None
+        self._prev_edge = None 
 
     def run(self):
         self._main_window.show()
         self.create_motion_graph(['data/02/02_02.bvh', 'data/02/02_03.bvh'])
         sys.exit(self._app.exec_())
-
 
     def load_bvh(self, fn):
         skeleton = AnimatedSkeleton()
@@ -65,17 +74,61 @@ class MotionViewerApp:
         if self._motion_graph is None:
             return
 
-        self._motion_graph_player.update(1.0 / 120.0)
+        if self._motion_player.motion is None:
+            self._motion_graph_player.update(UPDATE_DT)
+            edge, frame, transform = self._motion_graph_player.current_motion_data()
+            if self._prev_edge is not edge:
+                self._ui.graph_widget.visualizer.set_active_edge(edge)
+            self._prev_edge = edge
+            
+            motion = edge.motion
+        else:
+            self._motion_player.update(UPDATE_DT)
+            # TODO notify visualizer that motion graph is not being played anymore
+            motion, frame, transform = self._motion_player.current_motion_data()
 
-        edge, frame, transform = self._motion_graph_player.current_motion_data()
-
-        if self._prev_edge is not edge:
-            self._ui.graph_widget.visualizer.set_active_edge(edge)
-        self._prev_edge = edge
-
-        self._ui.viewer_widget.add_motion(edge.motion, transform)
+        self._ui.viewer_widget.add_motion(motion, transform)
         self._ui.viewer_widget.set_current_frame(frame)
         self._ui.viewer_widget.updateGL()
+
+    # https://gist.github.com/lschmierer/443b8e21ad93e2a2d7eb
+    def _setup_theme(self):
+        self._app.setStyle("Fusion")
+        dark_palette = QPalette()
+        dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+        dark_palette.setColor(QPalette.WindowText, QtCore.Qt.white)
+        dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+        dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+        dark_palette.setColor(QPalette.ToolTipBase, QtCore.Qt.white)
+        dark_palette.setColor(QPalette.ToolTipText, QtCore.Qt.white)
+        dark_palette.setColor(QPalette.Text, QtCore.Qt.white)
+        dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+        dark_palette.setColor(QPalette.ButtonText, QtCore.Qt.white)
+        dark_palette.setColor(QPalette.BrightText, QtCore.Qt.red)
+        dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+        dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+        dark_palette.setColor(QPalette.HighlightedText, QtCore.Qt.black)
+        self._app.setPalette(dark_palette)
+        self._app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+
+    def on_import_motion(self, path):
+        skeleton = AnimatedSkeleton()
+        try:
+            skeleton.load_from_file(path)
+            return skeleton
+        except (OSError, UnsupportedError, FormatError) as e:
+            self._warn('Import Motion', str(e))
+
+    def on_remove_motion(self, motion):
+        if self._motion_player.motion is motion:
+            self._motion_player.motion = None
+
+    def on_play_motion(self, motion):
+        self._motion_player.motion = motion
+
+    def _warn(self, title, msg):
+        QtWidgets.QMessageBox.warning(self._ui.main_window, title, msg)
+
 
 if __name__ == "__main__":
     app = MotionViewerApp()
